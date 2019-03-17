8ea3c119d5ece871c9aaba46352e77c830d41f74
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-03-17 21:05:00 +0300

    Add "add note" method

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-const validator = require("validator");
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const getNotes = require("./notes");
+// Customize yargs version
+yargs.version('1.1.0');
 
-const msg = getNotes();
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+});
 
-console.log(msg);
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+});
 
-console.log(validator.isEmail("test@mail.com"));
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+});
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+});
+
+yargs.parse();
